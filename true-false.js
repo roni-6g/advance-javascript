b865f4807b72be985b496ea099c94f50be06307d
roni-6g <mdroni710411@gmail.com>
2021-02-05 02:42:42 +0600

    Check True or false condition

---
+++ true-false.js
@@ -0,0 +1,25 @@
+// Falsy Condition
+// 0
+// ""
+// false
+// undefined
+// null
+// NaN
+
+// Truthy Condition
+// " "
+// "0"
+// []
+// {}
+
+
+
+let name = 0 ;
+
+if(name || name == 0){
+    console.log("Condition is True");
+
+}
+else {
+    console.log("Condition is False");
+}
