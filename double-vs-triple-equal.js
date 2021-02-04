d16eb34b3628768c6085d343330b1c87515a6a1e
roni-6g <mdroni710411@gmail.com>
2021-02-05 03:23:29 +0600

    check triple = (====) and check type with value

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+const num1 = 2;
+const num2 = '2';
+
+if(num1 === num2){
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
+
+// Check ==== condition and it is check type and value
