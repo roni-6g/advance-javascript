a5cc406cebef2f29be0b73ecdbcee8f72d9bf7d9
roni-6g <mdroni710411@gmail.com>
2021-02-05 22:55:24 +0600

    Advanced javascript

---
+++ array-split.js
@@ -0,0 +1,11 @@
+
+const nums = [10, 12, 8, 15, 16, 13, 20, 14];
+//const part1 = nums.slice(2, 5);
+// //console.log(part1);
+// const remove = nums.splice(2,3, 30,31,32);
+// console.log(remove);
+console.log(nums);
+
+const addElement = nums.join(",");
+
+console.log(addElement);
---
+++ scope.js
@@ -0,0 +1,15 @@
+
+let added = 10;
+
+function calculation(num1, num2){
+    let total = num1 + num2; 
+    if(total > 10){
+        const mood = "Happy Mood Now";
+        
+    }
+    console.log(mood);
+    return total;
+}
+
+const result = calculation(5,10);
+console.log(result);
